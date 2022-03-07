b7264b93c1760213ef85821597ec4f9e70feba58
Brendon Silva <96079864+Br3ndonn@users.noreply.github.com>
2022-03-07 00:00:08 -0300

    Update youtube-downloader.py

--- youtube-downloader.py
+++ youtube-downloader.py
@@ -1,7 +1,12 @@
 from pytube import YouTube
 
-DOWNLOAD_FOLDER = "/Users/brend/Downloads"
-video_url = "https://www.youtube.com/watch?v=EVv7BDGnONA"
-video_obj = YouTube(video_url)
-stream = video_obj.streams.get_highest_resolution()
-stream.download(DOWNLOAD_FOLDER)
+try:
+    # DEFINE PASTA Downloads COMO DESTINO
+    DOWNLOAD_FOLDER = "/Users/brend/Downloads"
+    video_url = input('Insira o link do vídeo: ')
+    video_obj = YouTube(video_url)
+    # SELECIONA A MELHOR DEFINIÇÃO DISPONÍVEL
+    stream = video_obj.streams.get_highest_resolution()
+    stream.download(DOWNLOAD_FOLDER)
+except:
+    print('ERRO: Verifique sua URL ou conexão com a internet.')
